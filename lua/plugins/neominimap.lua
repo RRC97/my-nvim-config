53f0baa9c0c80e3ccd7a56cefb4dfb136af6493c
RRC97 <matheuspereira.nave@gmail.com>
2025-07-04 22:01:21 -0300

    Removendo o neominimap de iniciar sozinho

--- lua/plugins/neominimap.lua
+++ lua/plugins/neominimap.lua
@@ -2,7 +2,7 @@
 return {
   "Isrothy/neominimap.nvim",
   version = "v3.x.x",
-  lazy = false, -- NOTE: NO NEED to Lazy load
+  -- lazy = false, -- NOTE: NO NEED to Lazy load
   -- Optional. You can alse set your own keybindings
   keys = {
     -- Global Minimap Controls
