224aebe74e2d13b3a7b6d3c82b1d83bc86c14bc8
RRC97 <matheuspereira.nave@gmail.com>
2025-07-04 21:41:16 -0300

    adicionando shortcut para fugitive

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -2,10 +2,16 @@ vim.keymap.set("n", "<leader>gs", "<cmd>G<CR>", { desc = "Fugitive Status"});
 vim.keymap.set('n', '<leader>gl', '<cmd>Gllog --stat --pretty=format:"[%ad] %h - %s (%an)" --date=iso-strict | wincmd p <CR>', { silent = true })
 vim.keymap.set('n', '<leader>gc', '<cmd>Gclog! | wincmd p<CR>', { silent = true })
 vim.keymap.set('n', '<leader>gd', '<cmd>Gdiff<CR>', { silent = true })
+vim.keymap.set('n', '<leader>gD', '<cmd>Gvdiffsplit<CR>', { silent = true })
+vim.keymap.set('n', '<leader>gC', '<cmd>Git commit<CR>', { silent = true })
+vim.keymap.set('n', '<leader>gP', '<cmd>Git push<CR>', { silent = true })
+vim.keymap.set('n', '<leader>gp', '<cmd>Git pull<CR>', { silent = true })
+-- Fugitive key mappings
+
 -- Próximo commit no Gdiff
-vim.keymap.set('n', '<leader>gn', ':Gvdiffsplit HEAD~1<CR>', { silent = true })
+vim.keymap.set('n', '<leader>gj', ':Gvdiffsplit HEAD~1<CR>', { silent = true })
 
 -- Commit anterior no Gdiff
-vim.keymap.set('n', '<leader>gp', ':Gvdiffsplit HEAD^<CR>', { silent = true })
+vim.keymap.set('n', '<leader>gk', ':Gvdiffsplit HEAD^<CR>', { silent = true })
 
 
