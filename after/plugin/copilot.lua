1dd2db2c58287543e65c84119ccb7dbd8066a226
RRC97 <matheuspereira.nave@gmail.com>
2025-08-24 21:54:42 -0300

    Corrigindo o blade e tirando avante e voltando copilot

--- after/plugin/copilot.lua
+++ after/plugin/copilot.lua
@@ -5,6 +5,30 @@ vim.g.copilot_filetypes = {
   [""] = false, -- buffers sem tipo
 }
 
+_G.copilot_enabled = true
+
+function ToggleCopilot()
+  if _G.copilot_enabled then
+    vim.cmd('Copilot disable');
+    _G.copilot_enabled = false
+    print("Copilot desativado")
+  else
+    vim.cmd('Copilot enable');
+    _G.copilot_enabled = true
+    print("Copilot ativado")
+  end
+end
+
+
+
+-- Map para <Leader>c ativar/desativar manualmente
+vim.api.nvim_set_keymap(
+  'n',
+  '<Leader>c',
+  '<Cmd>lua ToggleCopilot()<CR>',
+  { noremap = true, silent = true }
+)
+
 vim.cmd [[
   imap <silent><script><expr> <C-l> copilot#Accept("\<CR>")
 ]]
--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -25,9 +25,7 @@ vim.keymap.set('n', 'ZQ', ':qa!<CR>', { desc = 'Fechar tudo sem salvar' })
 -- ZX → salva tudo e sai (como :wa | qa)
 vim.keymap.set('n', 'ZX', ':wqa<CR>', { desc = 'Salvar tudo e sair' })
 
-vim.keymap.set("n", "<leader>cn", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<leader>cp", "<cmd>cprev<CR>zz")
-vim.keymap.set("n", "<leader>cq", function()
+vim.keymap.set("n", "<leader>q", function()
   -- if there are no diagnostics, do nothing or close quickfix opened
   local diagnostics = vim.diagnostic.get(0)
   if vim.tbl_isempty(diagnostics) then
@@ -38,7 +36,8 @@ vim.keymap.set("n", "<leader>cq", function()
   end
 end, { desc = "Abrir quickfix com diagnostics" })
 
-vim.keymap.set("n", "<leader>q", ":tabclose<CR>", { desc = "Close current tab" })
+vim.keymap.set("n", "<leader>k", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>cnext<CR>zz")
 
 vim.keymap.set("n", "<leader>ln", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>lp", "<cmd>lprev<CR>zz")
@@ -46,8 +45,6 @@ vim.keymap.set("n", "<leader>lp", "<cmd>lprev<CR>zz")
 vim.keymap.set("n", "<leader>rn", function()
   vim.o.relativenumber = not vim.o.relativenumber
 end, { desc = "Toggle relative number" })
-vim.keymap.set("n", "<C-CR>", "o<Esc>", { noremap = true, silent = true, desc = "Nova linha abaixo" })
-vim.keymap.set("n", "<C-S-CR>", "O<Esc>", { noremap = true, silent = true, desc = "Nova linha acima" })
 
 vim.keymap.set("n", "<leader><leader>", ":source $MYVIMRC<CR>", { desc = "Refresh config" })
 
