2afac2d444dc9559de80b5684d87b3f06f8b1768
RRC97 <matheuspereira.nave@gmail.com>
2026-06-20 11:27:35 -0300

    Auto session correção

--- lua/plugins/auto-session.lua
+++ lua/plugins/auto-session.lua
@@ -1,3 +1,5 @@
+local startup_cwd = vim.fn.getcwd()
+
 return {
   "rmagatti/auto-session",
   lazy = false,
@@ -16,7 +18,17 @@ return {
     },
     -- Hook essencial: Lê o arquivo Shada LOGO APÓS carregar a sessão
     post_restore_cmds = {
-      function() vim.cmd("rshada!") end
+      function() vim.cmd("rshada!") end,
+      function()
+        -- Restaura o CWD para o diretório em que o Neovim foi iniciado
+        vim.cmd("cd " .. vim.fn.fnameescape(startup_cwd))
+
+        -- Garante a sincronização do nvim-tree se o plugin estiver carregado
+        local has_tree, api = pcall(require, "nvim-tree.api")
+        if has_tree then
+          api.tree.change_root(startup_cwd)
+        end
+      end
     },
     session_lens = {
       picker = "telescope",
